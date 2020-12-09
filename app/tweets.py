5fd8e7a07ae6b08d33c265be52f214b0b8bdcaf4
lewis-munyi <lewismunyi97@gmail.com>
2020-12-09 17:51:12 +0300

    Added code Download tweets Save tweets to csv

--- app/tweets.py
+++ app/tweets.py
@@ -1,62 +1,87 @@
-import tweepy
+import tweepy # https://github.com/tweepy/tweepy
+from dotenv import load_dotenv
 import os
-from dotenv import load_dotenv()
-
+import csv
+load_dotenv()
 class Tweet:
     # Visit https://developer.twitter.com/en/apps/ to get your keys
     api_key = ""
     api_secret = ""
     access_token = ""
     access_secret = ""
-    username = ""
+    username = ''
+    tweets = None
 
     def __init__(self, username):
-        self.username = username
+        if username == "":
+            print('Supply a correct username')
+        else:
+            self.api_key = os.getenv("api_key")
+            self.api_secret = os.getenv("api_secret")
+            self.access_token = os.getenv("access_token")
+            self.access_secret = os.getenv("access_secret")
+            self.username = username
 
-    def fetch(self, max):      
+    def fetch(self):
+        print("Starting engines...")
         # Twitter only allows access to a users most recent 3240 tweets with this method
-        
+
         # Authorize twitter, initialize tweepy
-            auth = tweepy.OAuthHandler(api_key, api_secret)
-            auth.set_access_token(access_token, access_secret)
-            api = tweepy.API(auth)
-        
+        auth = tweepy.OAuthHandler(self.api_key, self.api_secret)
+        auth.set_access_token(self.access_token, self.access_secret)
+        api = tweepy.API(auth)
+
         # Initialize a list to hold all the tweepy Tweets
-            alltweets = []
-        
+        alltweets = []
+
         # Remove '@' sign if present
-        if(screen_name[0] == "@"):
-                screen_name = screen_name[1:]
+        if(self.username[0] == "@"):
+            self.username = self.username[1:]
+
+        print("Fetching tweets ...")
         # Make initial request for most recent tweets (200 is the maximum allowed count)
-            new_tweets = api.user_timeline(screen_name, count=200)
-        
+        new_tweets = api.user_timeline(self.username, count=200)
+
         # Save most recent tweets
-            alltweets.extend(new_tweets)
-        
+        alltweets.extend(new_tweets)
+
         # Save the id of the oldest tweet
-            oldest = alltweets[-1].id - 1
-        
+        oldest = alltweets[-1].id - 1
+
         # Keep fetching tweets until there are no tweets left to grab or until we reach 3240 tweets
-        whilelen(new_tweets) > 0:
-        # print("\tFetching tweets before %s" % (oldest))
+        while len(new_tweets) > 0:
+            # print("\tFetching tweets before %s" % (oldest))
 
-        # All subsequent requests use the max_id param to prevent duplicates
-                new_tweets = api.user_timeline(screen_name, count=200, max_id=oldest)
+            # All subsequent requests use the max_id param to prevent duplicates
+            new_tweets = api.user_timeline(self.username, count=200, max_id=oldest)
 
-        # Save most recent tweets
-                alltweets.extend(new_tweets)
-        
-        # Update the id of the oldest tweet less one
-                oldest = alltweets[-1].id - 1
+            # Save most recent tweets
+            alltweets.extend(new_tweets)
 
-        print("\t...%s tweets downloaded so far" % (len(alltweets)))
-        
+            # Update the id of the oldest tweet less one
+            oldest = alltweets[-1].id - 1
+
+            print("\t...%s tweets downloaded so far" % (len(alltweets)))
+
+        self.tweets = alltweets
+
+        print("Finished fetching tweets.\n\n")
+        return self.processTweets()
+
+    def processTweets(self):
+        print('Processing tweets...')
+
         # Transform the tweepy tweets into a 2D array that will populate the csv
-        # To-do: fix missing keys error !if retweet
-        # outtweets = [[tweet.id, tweet.created_at, emoji.demojize(tweet.text), tweet.text, tweet.source, tweet.retweet_count, tweet.favorite_count, tweet.retweeted, tweet.retweeted_status.user.screen_name, tweet.retweeted_status.user.id, tweet.retweeted_status.id, tweet.lang, "" ] for tweet in alltweets]
-            outtweets = [[tweet.id_str, tweet.created_at, emoji.demojize(tweet.text), tweet.text, tweet.source, tweet.retweet_count, tweet.favorite_count, tweet.retweeted, tweet.user.id, tweet.user.name, tweet.user.screen_name, tweet.user.description, tweet.lang,""]for tweet in alltweets]
+        outtweets = [[tweet.text.encode("utf-8"), tweet.user.description, tweet.lang ] for tweet in self.tweets]
 
+        print(f"\tWriting {len(self.tweets)} tweets {self.username}.csv\n")
 
+        with open(f"tweets/{self.username}.csv", 'w') as f:
+            writer = csv.writer(f)
 
-    def save(self, tweets):
-        # Save tweets
+            # Write CSV Header
+            writer.writerow(["tweet", "bio", "language"])
+
+            # Populate CSV with tweets
+            writer.writerows(outtweets)
+        print("Done!")
